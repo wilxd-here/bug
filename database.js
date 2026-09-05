724bdf7a22f7258d9e5bdc29ebcc0ffc83510113
wilxd-here <alfatihwildan219@gmail.com>
2026-09-05 15:28:18 +0700

    Add files via upload

--- database.js
+++ database.js
@@ -6,7 +6,7 @@
 
 const databaseAkun = [
     { username: "wilzie", password: "123", role: "owner" },
-    { username: "Ambatron", password: "123", role "member" },
+    { username: "Ambatron", password: "123", role: "member" },
     // Kamu bisa tambah akun pembeli baru di bawah sini:
     // { username: "nama_pembeli", password: "password_nya", role: "member" },
 ];
