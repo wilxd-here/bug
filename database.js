263e3f8bdd5aa299852d592ef6c77752180c0f19
wilxd-here <alfatihwildan219@gmail.com>
2026-09-05 15:24:31 +0700

    Add files via upload

---
+++ database.js
@@ -0,0 +1,12 @@
+// ==========================================
+// DATABASE AKUN XAERISOFT VOID
+// Urutan Role (terendah ke tertinggi): 
+// 'member' -> 'reseller' -> 'partner' -> 'admin' -> 'owner'
+// ==========================================
+
+const databaseAkun = [
+    { username: "wilzie", password: "123", role: "owner" },
+    { username: "Ambatron", password: "123", role "member" },
+    // Kamu bisa tambah akun pembeli baru di bawah sini:
+    // { username: "nama_pembeli", password: "password_nya", role: "member" },
+];
